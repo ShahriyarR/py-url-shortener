4a0dff4c3e244a72b545d093e93c43081d8a907b
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2024-12-08 21:32:23 +0100

    feat: use pydantic models as domain models and only with 3 fields: original_url, short_url and the created_by

--- src/pyurlshortener/domain/models.py
+++ src/pyurlshortener/domain/models.py
@@ -1,17 +1,11 @@
-from dataclasses import dataclass
-from datetime import datetime
+from pydantic import BaseModel, HttpUrl
 
 
-@dataclass
-class Url:
-    short_url: str
-    original_url: str
-    creation_date: datetime
-    expiration_date: datetime
+class Url(BaseModel):
+    short_url: HttpUrl
+    original_url: HttpUrl
     created_by: str
 
 
-def url_factory(
-    short_url: str, original_url: str, creation_date: datetime, expiration_date: datetime, created_by: str
-) -> Url:
-    return Url(short_url, original_url, creation_date, expiration_date, created_by)
+def url_factory(short_url: HttpUrl, original_url: HttpUrl, created_by: str) -> Url:
+    return Url(short_url=short_url, original_url=original_url, created_by=created_by)
--- tests/domain/test_models.py
+++ tests/domain/test_models.py
@@ -1,4 +1,8 @@
 from datetime import datetime, timedelta
+from xml.dom import ValidationErr
+
+import pytest
+from pydantic import ValidationError
 
 from pyurlshortener.domain.models import Url, url_factory
 
@@ -6,34 +10,35 @@ from pyurlshortener.domain.models import Url, url_factory
 def test_url_creation():
     short_url = "http://short.url/abc123"
     original_url = "http://original.url/longurl"
-    creation_date = datetime.now()
-    expiration_date = creation_date + timedelta(days=30)
     created_by = "user123"
 
     # Create a Url instance
-    url = Url(short_url, original_url, creation_date, expiration_date, created_by)
+    url = Url(short_url=short_url, original_url=original_url, created_by=created_by)
 
     # Assertions
-    assert url.short_url == short_url
-    assert url.original_url == original_url
-    assert url.creation_date == creation_date
-    assert url.expiration_date == expiration_date
+    assert str(url.short_url) == short_url
+    assert str(url.original_url) == original_url
     assert url.created_by == created_by
 
 
 def test_url_factory():
     short_url = "http://short.url/xyz789"
     original_url = "http://original.url/somelongurl"
-    creation_date = datetime.now()
-    expiration_date = creation_date + timedelta(days=60)
     created_by = "user456"
 
     # Use the factory to create a Url instance
-    url = url_factory(short_url, original_url, creation_date, expiration_date, created_by)
-
+    url = url_factory(short_url, original_url, created_by)
     # Assertions
-    assert url.short_url == short_url
-    assert url.original_url == original_url
-    assert url.creation_date == creation_date
-    assert url.expiration_date == expiration_date
+    assert str(url.short_url) == short_url
+    assert str(url.original_url) == original_url
     assert url.created_by == created_by
+
+
+def test_url_factory_with_wrong_type():
+    short_url = "google.com"
+    original_url = "http://original.url/somelongurl"
+    created_by = "user456"
+
+    # Use the factory to create a Url instance
+    with pytest.raises(ValidationError):
+        url_factory(short_url, original_url, created_by)
