e77c6f4c0f5ab04a6e1183d617d08e8f805fac13
Shahriyar Rzayev <shahriyar.rzayev@nordsec.com>
2024-12-10 16:14:17 +0100

    feat: add url shortener service

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is an educational project, based on the [System Design Walkthrough by Hello
 
 The ultimate goal is to show the full implementation of the project, in a small iterations using Python.
 
-This will close the gap between theoritical System Design and the real implementation - and will show how the decisions are changing with each iteration.
+This will close the gap between theoretical System Design and the real implementation - and will show how the decisions are changing with each iteration.
 
 ## Setup
 
---
+++ src/pyurlshortener/service/url.py
@@ -0,0 +1,13 @@
+from pydantic import HttpUrl
+
+from pyurlshortener.domain.models import Url, url_factory
+from pyurlshortener.service.counter import ThreadUnsafeCounter
+from pyurlshortener.service.encoder import encode
+
+
+def shortened(original_url: str, created_by: str, counter: ThreadUnsafeCounter) -> Url:
+    counter.increment()
+    val = counter.value()
+    encoded = encode(val)
+    encoded_url = f"https://short.url/{encoded}"
+    return url_factory(short_url=HttpUrl(encoded_url), original_url=HttpUrl(original_url), created_by=created_by)
